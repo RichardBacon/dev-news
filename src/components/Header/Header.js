5fe951f3bd651e96913a65a80432b7b26e8fd106
Richard Bacon <richardjohnbacon@gmail.com>
2021-01-23 13:51:00 +0000

    Update header content and styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from '@reach/router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Header = (props) => {
-  const { username } = props;
-
+const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <Link to="/" aria-label="home">
-          <FontAwesomeIcon className={styles.logo} icon="newspaper" />
-        </Link>
-        <h1 className={styles.title}>Dev News</h1>
-        <p className={styles.username}>{username}</p>
-      </div>
+      <Link className={styles.link} to="/" aria-label="home">
+        Dev News
+      </Link>
     </header>
   );
 };
