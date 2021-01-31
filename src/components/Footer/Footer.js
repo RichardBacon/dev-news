1349be44ce4849c4d3232bfa4e3f44fd5c121a00
Richard Bacon <richardjohnbacon@gmail.com>
2021-01-31 14:42:09 +0000

    Update footer markup for semantics

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <footer className={styles.footer}>&copy; Dev News 2021</footer>;
+  return (
+    <footer className={styles.footer}>
+      <small>&copy; Dev News 2021</small>
+    </footer>
+  );
 };
 
 export default Footer;
