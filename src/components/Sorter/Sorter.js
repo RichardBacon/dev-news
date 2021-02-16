2911fde1030eef737ae5fae95fe8999838133457
Richard Bacon <richardjohnbacon@gmail.com>
2021-02-16 15:26:13 +0000

    Update select input styling to be consistent

--- src/components/Sorter/Sorter.js
+++ src/components/Sorter/Sorter.js
@@ -6,30 +6,34 @@ const Sorter = ({ sort_by, order, handleInputChange }) => {
     <form className={styles.form}>
       <p className={styles.formLabel}>Sorted By:</p>
       <label className={styles.label} htmlFor="sort_by" aria-label="sort_by">
-        <select
-          className={styles.selector}
-          value={sort_by}
-          name="sort_by"
-          id="sort_by"
-          onChange={handleInputChange}
-        >
-          <option value="comment_count">Comments</option>
-          <option value="created_at">Date</option>
-          <option value="votes">Likes</option>
-        </select>
+        <div className={styles.selectContainer}>
+          <select
+            className={styles.selector}
+            value={sort_by}
+            name="sort_by"
+            id="sort_by"
+            onChange={handleInputChange}
+          >
+            <option value="comment_count">Comments</option>
+            <option value="created_at">Date</option>
+            <option value="votes">Likes</option>
+          </select>
+        </div>
       </label>
 
       <label className={styles.label} htmlFor="order" aria-label="order">
-        <select
-          className={styles.selector}
-          value={order}
-          name="order"
-          id="order"
-          onChange={handleInputChange}
-        >
-          <option value="asc">Ascending</option>
-          <option value="desc">Descending</option>
-        </select>
+        <div className={styles.selectContainer}>
+          <select
+            className={styles.selector}
+            value={order}
+            name="order"
+            id="order"
+            onChange={handleInputChange}
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
       </label>
     </form>
   );
