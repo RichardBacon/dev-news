729e532bd3bdee2e752d09dbe7e0a760eca7b7b0
Richard Bacon <richardjohnbacon@gmail.com>
2021-02-16 15:27:10 +0000

    Prevent ios devices zooming on select input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,10 @@ class App extends Component {
           <html lang="en" />
           <title>Dev News</title>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
         </Helmet>
 
         <PageLayout user={user} />
