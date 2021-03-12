d981e5b598dd22779f5ac84d482633ed41e9edb7
Richard Bacon <richardjohnbacon@gmail.com>
2021-03-12 14:53:14 +0000

    Convert TopicList to functional component

--- src/components/TopicList/TopicList.js
+++ src/components/TopicList/TopicList.js
@@ -1,66 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as api from '../../utils/api';
 import styles from './TopicList.module.css';
 import TopicCard from '../TopicCard/TopicCard';
 import Loader from '../Loader/Loader';
 import ErrorDisplayer from '../ErrorDisplayer/ErrorDisplayer';
 
-class TopicList extends Component {
-  state = {
-    topics: [],
-    isLoading: true,
-    err: null,
-  };
+const TopicList = () => {
+  const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState(null);
 
-  render() {
-    const { topics, isLoading, err } = this.state;
-
-    if (err) return <ErrorDisplayer {...err} />;
-
-    return (
-      <>
-        <header className={styles.header}>
-          <h1 className={styles.title}>Topics</h1>
-        </header>
-
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <ul>
-            {topics.map((topic) => {
-              const { title } = topic;
-
-              return (
-                <li key={title}>
-                  <TopicCard {...topic} />
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </>
-    );
-  }
-
-  componentDidMount() {
-    this.getTopics();
-  }
-
-  getTopics = () => {
-    this.setState({ isLoading: true });
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setIsLoading(true);
 
     api
       .fetchTopics()
       .then((topics) => {
-        this.setState({ topics, isLoading: false });
+        setTopics(topics);
       })
       .catch((err) => {
-        this.setState({
-          err: { msg: err.response.data.msg, status: err.response.status },
-          isLoading: false,
-        });
+        setErr({ msg: err.response.data.msg, status: err.response.status });
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
-  };
-}
+  }, []);
+
+  if (err) return <ErrorDisplayer {...err} />;
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Topics</h1>
+      </header>
+
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ul>
+          {topics.map((topic) => {
+            const { title } = topic;
+
+            return (
+              <li key={title}>
+                <TopicCard {...topic} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
+};
 
 export default TopicList;
