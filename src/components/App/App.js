6ef2d3147129c66f41284f1172ecd8706c94c15a
Richard Bacon <richardjohnbacon@gmail.com>
2021-03-12 09:35:21 +0000

    Convert App to functional component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import './App.module.css';
 import PageLayout from '../PageLayout/PageLayout';
-class App extends Component {
-  state = {
-    user: {
-      username: 'TopCoder',
-    },
+
+const App = () => {
+  const user = {
+    username: 'TopCoder',
   };
 
-  render() {
-    const { user } = this.state;
+  return (
+    <>
+      <Helmet>
+        <html lang="en" />
+        <title>Dev News</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Helmet>
 
-    return (
-      <>
-        <Helmet>
-          <html lang="en" />
-          <title>Dev News</title>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
-          />
-        </Helmet>
-
-        <PageLayout user={user} />
-      </>
-    );
-  }
-}
+      <PageLayout user={user} />
+    </>
+  );
+};
 
 export default App;
